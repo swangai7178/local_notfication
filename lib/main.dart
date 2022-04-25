774ea15d67435d8363f5282410417708cdec38d8
samuel wangai <44896330+swangai7178@users.noreply.github.com>
2022-04-25 15:30:59 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: unnecessary_new, unnecessary_string_interpolations
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+void main() => runApp(new MaterialApp(home: new MyApp()));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    var android = const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iOS = const IOSInitializationSettings();
+    var initSetttings = new InitializationSettings(android: android, iOS: iOS);
+    flutterLocalNotificationsPlugin.initialize(initSetttings,
+      );
+  }
+
+  Future 
+  
+  
+  onSelectNotification(String payload) async {
+    debugPrint("payload : $payload");
+    showDialog(
+      context: context,
+      builder: (_) => new AlertDialog(
+        title: const Text('Notification'),
+        content: new Text('$payload'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: const Text('Flutter Local Notification'),
+      ),
+      body: new Center(
+        child: new RaisedButton(
+          onPressed: showNotification,
+          child: new Text(
+            'new new new',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+        ),
+      ),
+    );
+  }
+
+  showNotification() async {
+    var android = const AndroidNotificationDetails(
+        'channel id', 'channel NAME''', 
+        priority: Priority.high,importance: Importance.max
+    );
+    var iOS = const IOSNotificationDetails();
+    var platform = new NotificationDetails(android: android, iOS: iOS);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'New Video is out', 'samuel is me and new', platform,
+        payload: 'Nitish Kumar Singh is part time Youtuber');
+  }
+}
